17ec3330b65cfdc141b2b87ed9fa09aa5bc22e52
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-06 05:21:07 +0000

    Added the OpenEhs.Web project as well as it's associated unit test project.

---
+++ trunk/code/src/app/OpenEhs.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenEhs.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kaizen Consulting")]
+[assembly: AssemblyProduct("OpenEhs.Web")]
+[assembly: AssemblyCopyright("Copyright © 2011 Kaizen Consulting")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64fdabe3-b190-48c3-8739-c94f5f1066e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
