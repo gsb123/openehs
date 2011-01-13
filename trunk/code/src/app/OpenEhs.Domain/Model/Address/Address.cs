7fad8391d3c3582046b76a7850a7cdfc30f1937d
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-13 05:37:53 +0000

    Populated LogIn and Address

--- trunk/code/src/app/OpenEhs.Domain/Model/Address/Address.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Address/Address.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*****************************************************************************
+ * Project: Open Electronic Healthcare System
+ * Group: Ghana Team
+ * Date: Jan-12-2011
+ * 
+ * Author: Cameron Harp (charp5257@gmail.com)
+ *****************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +15,69 @@ namespace OpenEhs.Domain
 {
     public class Address
     {
+        #region Fields
 
+        private int _id;
+        private string _street1;
+        private string _street2;
+        private string _city;
+        private string _region;
+        private string _country;
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Street1
+        {
+            get { return _street1; }
+            set { _street1 = value; }
+        }
+
+        public string Street2
+        {
+            get { return _street2; }
+            set { _street2 = value; }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public string Region
+        {
+            get { return _region; }
+            set { _region = value; }
+        }
+
+        public string Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
+
+        #endregion
+
+        #region Constructor(s)
+
+        public Address(int id, string street1, string street2, string city, string region, string country)
+        {
+            Id = id;
+            Street1 = street1;
+            Street2 = street2;
+            City = city;
+            Region = region;
+            Country = country;
+        }
+
+        #endregion
     }
 }
--- trunk/code/src/app/OpenEhs.Domain/Model/LogIn/LogIn.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/LogIn/LogIn.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*****************************************************************************
+ * Project: Open Electronic Healthcare System
+ * Group: Ghana Team
+ * Date: Jan-12-2011
+ * 
+ * Author: Cameron Harp (charp5257@gmail.com)
+ *****************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +15,53 @@ namespace OpenEhs.Domain
 {
     public class LogIn
     {
+        #region Fields
 
+        private int _id;
+        private string _username;
+        private string _password;
+        private string _loginpermissions;
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string UserName
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string LogInPermissions
+        {
+            get { return _loginpermissions; }
+            set { _loginpermissions = value; }
+        }
+
+        #endregion
+
+        #region Constructor(s)
+
+        public LogIn(int id, string username, string password, string loginpermissions)
+        {
+            Id = id;
+            UserName = username;
+            Password = password;
+            LogInPermissions = loginpermissions;
+        }
+
+        #endregion
     }
 }
