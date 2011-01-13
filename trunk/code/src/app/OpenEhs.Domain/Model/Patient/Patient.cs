dedc310d97b275943d1aa87c891837328930e2a2
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-13 05:20:58 +0000

    Populated Staff and Patinet classes in the domain!!!

--- trunk/code/src/app/OpenEhs.Domain/Model/Patient/Patient.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Patient/Patient.cs
@@ -1,6 +1,113 @@
-﻿namespace OpenEhs.Domain
+﻿using System;
+
+namespace OpenEhs.Domain
 {
     public class Patient
     {
+        #region Fields
+
+        private int _id;
+        private string _firstname;
+        private string _middlename;
+        private string _lastname;
+        private DateTime _dateofbirth;
+        private Gender _gender;
+        private string _phonenumber;
+        private Address _address;
+        private string _bloodtype;
+        private string _triberace;
+        private string _religion;
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        public string MiddleName
+        {
+            get { return _middlename; }
+            set { _middlename = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateofbirth; }
+            set { _dateofbirth = value; }
+        }
+
+        public Gender Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phonenumber; }
+            set { _phonenumber = value; }
+        }
+
+        public Address Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public string BloodType
+        {
+            get { return _bloodtype; }
+            set { _bloodtype = value; }
+        }
+
+        public string TribeRace
+        {
+            get { return _triberace; }
+            set { _triberace = value; }
+        }
+
+        public string Religion
+        {
+            get { return _religion; }
+            set { _religion = value; }
+        }
+
+        #endregion
+
+        #region Constructor(s)
+
+        public Patient(int id, string firstname, string middlename, string lastname, DateTime dateofbirth, Gender gender, string phonenumber, Address address,
+            string bloodtype, string triberace, string religion)
+        {
+            Id = id;
+            FirstName = firstname;
+            MiddleName = middlename;
+            LastName = lastname;
+            DateOfBirth = dateofbirth;
+            Gender = gender;
+            PhoneNumber = phonenumber;
+            Address = address;
+            BloodType = bloodtype;
+            TribeRace = triberace;
+            Religion = religion;
+        }
+
+        #endregion
     }
 }
--- trunk/code/src/app/OpenEhs.Domain/Model/Staff/Staff.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Staff/Staff.cs
@@ -7,6 +7,93 @@ namespace OpenEhs.Domain
 {
     public class Staff
     {
+        #region Fields
 
+        private int _id;
+        private string _firstname;
+        private string _middlename;
+        private string _lastname;
+        private string _phonenumber;
+        private int _stafftype;
+        private string _licensenumber;
+        private Address _address;
+        private LogIn _login;
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        public string MiddleName
+        {
+            get { return _middlename; }
+            set { _middlename = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phonenumber; }
+            set { _phonenumber = value; }
+        }
+
+        public int StaffType
+        {
+            get { return _stafftype; }
+            set { _stafftype = value; }
+        }
+
+        public string LicenseNumber
+        {
+            get { return _licensenumber; }
+            set { _licensenumber = value; }
+        }
+
+        public Address Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public LogIn LogIn
+        {
+            get { return _login; }
+            set { _login = value; }
+        }
+
+        #endregion
+
+        #region Constructor(s)
+
+        public Staff(int id, string firstname, string middlename, string lastname, string phonenumber, int stafftype, string licensenumber, Address address, LogIn login)
+        {
+            Id = id;
+            FirstName = firstname;
+            MiddleName = middlename;
+            LastName = lastname;
+            PhoneNumber = phonenumber;
+            StaffType = stafftype;
+            LicenseNumber = licensenumber;
+            Address = address;
+            LogIn = login;
+        }
+
+        #endregion
     }
 }
