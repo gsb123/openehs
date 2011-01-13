b481609888a53b2b4045c8ccf63ab30c615a1b2d
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-13 22:44:17 +0000

    Added EmergencyContact/Allergy and more enums!

--- code/src/app/OpenEhs.Domain/Model/Patient/EmergencyContact.cs
+++ code/src/app/OpenEhs.Domain/Model/Patient/EmergencyContact.cs
@@ -7,5 +7,77 @@ namespace OpenEhs.Domain
 {
     public class EmergencyContact
     {
+        #region Fields
+
+        private int _id;
+        private string _firstname;
+        private string _lastname;
+        private string _phonenumber;
+        private Relationship _relationship;
+        private Address _addressid;
+        private Patient _patientid;
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phonenumber; }
+            set { _phonenumber = value; }
+        }
+
+        public Relationship Relationship
+        {
+            get { return _relationship; }
+            set { _relationship = value; }
+        }
+
+        public Address AddressID
+        {
+            get { return _addressid; }
+            set { _addressid = value; }
+        }
+
+        public Patient PatientID
+        {
+            get { return _patientid; }
+            set { _patientid = value; }
+        }
+
+        #endregion
+
+        #region Constructor(s)
+
+        public EmergencyContact(int id, string firstname, string lastname, string phonenumber, Relationship relationship, Address addressid, Patient patientid)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            PhoneNumber = phonenumber;
+            Relationship = relationship;
+            AddressID = addressid;
+            PatientID = patientid;
+        }
+
+        #endregion
     }
 }
---
+++ code/src/app/OpenEhs.Domain/Model/Patient/Relationship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenEhs.Domain
+{
+    public enum Relationship
+    {
+        Spouce = 0,
+        Father = 1,
+        Mother = 2,
+        Sibling = 3,
+        Ohter = 4
+    }
+}
