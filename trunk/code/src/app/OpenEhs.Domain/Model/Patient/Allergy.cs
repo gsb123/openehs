9b1f4754b016c97036acb9be42fab253098b2249
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-13 22:44:17 +0000

    Added EmergencyContact/Allergy and more enums!

--- trunk/code/src/app/OpenEhs.Domain/Model/Patient/Allergy.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Patient/Allergy.cs
@@ -7,5 +7,53 @@ namespace OpenEhs.Domain
 {
     public class Allergy
     {
+        #region Fields
+
+        private int _id;
+        private string _name;
+        private string _medication;
+        private Patient _patientid;
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Medication
+        {
+            get { return _medication; }
+            set { _medication = value; }
+        }
+
+        public Patient PatientID
+        {
+            get { return _patientid; }
+            set { _patientid = value; }
+        }
+
+        #endregion
+
+        #region Constructor(s)
+
+        public Allergy(int id, string name, string medication, Patient patientid)
+        {
+            Id = id;
+            Name = name;
+            Medication = medication;
+            PatientID = patientid;
+        }
+
+        #endregion
     }
 }
