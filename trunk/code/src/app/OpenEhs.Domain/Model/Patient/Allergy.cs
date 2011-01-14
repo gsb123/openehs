29852f57aa7f1674960555a024e0eae4c4c6b76f
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-14 18:21:50 +0000

    Added in headers and added classes for exceptions

--- trunk/code/src/app/OpenEhs.Domain/Model/Patient/Allergy.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Patient/Allergy.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*****************************************************************************
+ * Project: Open Electronic Healthcare System
+ * Group: Ghana Team
+ * Date: Jan-12-2011
+ * 
+ * Author: Cameron Harp (charp5257@gmail.com)
+ *****************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
