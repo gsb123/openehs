fb7c225a8329670b44a085e7aede5bfe7a17dcd1
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-17 00:09:01 +0000

    Added a few repository interfaces and did a couple of changes to the Product class (spelling and casing mistakes).

---
+++ code/src/app/OpenEhs.Data/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace OpenEhs.Data
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ code/src/app/OpenEhs.Data/Interfaces/IStaffRepository.cs
@@ -0,0 +1,8 @@
+﻿using OpenEhs.Domain;
+
+namespace OpenEhs.Data
+{
+    public interface IStaffRepository : IRepository<Staff>
+    {
+    }
+}
