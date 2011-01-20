74de29cba07d1f02d520c7af78173c04579d4bc2
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-20 04:11:17 +0000

    Renamed LogIn to User and added the Role class.

---
+++ trunk/code/src/app/OpenEhs.Domain/Model/LogIn/User.cs
@@ -0,0 +1,35 @@
+﻿/*****************************************************************************
+ * Project: Open Electronic Healthcare System
+ * Group: Ghana Team
+ * Date: Jan-12-2011
+ * 
+ * Author: Cameron Harp (charp5257@gmail.com)
+ *****************************************************************************/
+
+namespace OpenEhs.Domain
+{
+    public class User
+    {
+        #region Properties
+
+        public int Id { get; private set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        #endregion
+
+
+        #region Constructor(s)
+
+        public User(int id, string username, string password)
+        {
+            Id = id;
+            UserName = username;
+            Password = password;
+        }
+
+        #endregion
+    }
+}
