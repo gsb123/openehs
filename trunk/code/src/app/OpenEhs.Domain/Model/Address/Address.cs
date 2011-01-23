7e37bcd328b4805330433e5b80388b26f5ef6bea
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-23 19:59:51 +0000

    Marking members as virtual for ORM solution.

--- trunk/code/src/app/OpenEhs.Domain/Model/Address/Address.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Address/Address.cs
@@ -6,38 +6,18 @@
  * Author: Cameron Harp (charp5257@gmail.com)
  *****************************************************************************/
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace OpenEhs.Domain
 {
     public class Address
     {
         #region Properties
 
-        public int Id { get; private set; }
-        public string Street1 { get; set; }
-        public string Street2 { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string Country { get; set; }
-
-        #endregion
-
-
-        #region Constructor(s)
-
-        public Address(int id, string street1, string street2, string city, string region, string country)
-        {
-            Id = id;
-            Street1 = street1;
-            Street2 = street2;
-            City = city;
-            Region = region;
-            Country = country;
-        }
+        public virtual int Id { get; private set; }
+        public virtual string Street1 { get; set; }
+        public virtual string Street2 { get; set; }
+        public virtual string City { get; set; }
+        public virtual string Region { get; set; }
+        public virtual string Country { get; set; }
 
         #endregion
     }
--- trunk/code/src/app/OpenEhs.Domain/Model/Billing/InvoiceLineItem.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Billing/InvoiceLineItem.cs
@@ -10,64 +10,21 @@ namespace OpenEhs.Domain
 {
     public class InvoiceLineItem
     {
-        #region Fields
-
-        private int _id;
-        private Product _product;
-        private Service _service;
-        private decimal _quantity; // NOTE: So, is there a reason for this to be a floating point number?
-
-        #endregion
-
-
         #region Properties
 
-        public int Id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
-
-        public Product Product
-        {
-            get { return _product; }
-            set { _product = value; }
-        }
-
-        public Service Service
-        {
-            get { return _service; }
-            set { _service = value; }
-        }
-
-        public decimal Quantity
-        {
-            get { return _quantity; }
-            set { _quantity = value; }
-        }
-
+        public virtual int Id { get; private set; }
+        public virtual Product Product { get; set; }
+        public virtual Service Service { get; set; }
+        public virtual decimal Quantity { get; set; }
         public decimal Total
         {
             get
             {
-                if (_service != null)
-                    return _service.Price;
+                if (Service != null)
+                    return Service.Price;
 
-                return _product.Price * Quantity;
+                return Product.Price * Quantity;
             }
-        }
-
-        #endregion
-
-
-        #region Constructor(s)
-
-        public InvoiceLineItem(int id, Product product, Service service, decimal quantity)
-        {
-            Id = id;
-            Product = product;
-            Service = service;
-            Quantity = quantity;
         }
 
         #endregion
