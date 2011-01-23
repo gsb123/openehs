73af0c79a09f49cb80e178e4fe67bc5d62a71553
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-23 04:40:39 +0000

    Demo on how to use stored procs

---
+++ code/sql/Demo/Demo.Data/DataTier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Demo.Domain;
+using MySql.Data.MySqlClient;
+
+namespace Demo.Data
+{
+    public class DataTier
+    {
+        private const string ConnectionString = "server=localhost;" +
+                                           "user id=OpenEHS_admin;" +
+                                           "password=password;" +
+                                           "database=OpenEHS_database;" +
+                                           "pooling=false;";
+
+        public void InsertProduct(Products prod)
+        {
+            string procName = "sp_insertProducts";
+            string connectionString = "Server=localhost;Database=OpenEHS_database;Uid=OpenEHS_admin;Pwd=password;";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand(procName, conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new MySqlParameter("Name", prod.Name));
+                    cmd.Parameters.Add(new MySqlParameter("Unit", prod.Unit));
+                    cmd.Parameters.Add(new MySqlParameter("Catagory", prod.Catagory));
+                    cmd.Parameters.Add(new MySqlParameter("ProductCost", prod.ProductCost));
+                    cmd.Parameters.Add(new MySqlParameter("QuantityOnHand", prod.QuantityOnHand));
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+    }
+}
---
+++ code/sql/Demo/Demo.Pres/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Demo.Domain;
+using Demo.Data;
+using MySql.Data.MySqlClient;
+
+namespace Demo.Pres
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DataTier dt;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dt = new DataTier();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Products products = new Products();
+
+            products.Name = tbname.Text.ToString();
+            products.Unit = tbunit.Text.ToString();
+            products.Catagory = tbcata.Text.ToString();
+            products.ProductCost = Convert.ToDecimal(tbprodcost.Text.ToString());
+            products.QuantityOnHand = Convert.ToInt32(tbquanonhand.Text.ToString());
+
+            dt.InsertProduct(products);
+        }
+    }
+}
