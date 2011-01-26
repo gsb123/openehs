779a99b9c8f87e882eba08796987411f54a3e62e
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-26 04:09:59 +0000

    Added NHibernate libraries.

---
+++ trunk/code/src/app/OpenEhs.Data/Repositories/TemplateRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using OpenEhs.Domain;
+
+namespace OpenEhs.Data
+{
+    public class TemplateRepository : ITemplateRepository
+    {
+        public ITemplate Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ITemplate> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(ITemplate entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(ITemplate entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<ITemplate> Find(Expression<Func<ITemplate, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ITemplate> GetAllSurgeryTemplates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ITemplate> GetAllDiagnosisTemplates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ITemplate> GetAllNoteTemplates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ITemplate> GetAllReasonTemplates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
