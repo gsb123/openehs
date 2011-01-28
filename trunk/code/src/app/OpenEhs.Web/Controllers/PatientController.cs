0b851ee97d43bfe76d656a171a9307815355516d
austynmahoney <austynmahoney@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-28 06:36:19 +0000

    Starting to mess around with UI

---
+++ trunk/code/src/app/OpenEhs.Web/Controllers/PatientController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenEhs.Web.Controllers {
+    public class PatientController : Controller {
+        //
+        // GET: /Patient/
+
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult NewPatient() {
+            return View();
+        }
+
+        public ActionResult SearchPatients() {
+            return View();
+        }
+
+    }
+}
