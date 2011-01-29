d49b6a2973d8b6f52e086f40f661701601f08f16
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-29 06:29:41 +0000

    Added some organization to the OpenEhs.Data.Tests project as well as started development on the Unit of Work pattern.

---
+++ trunk/code/src/tests/OpenEhs.Data.Tests/Unit of Work/UnitOfWorkTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace OpenEhs.Data.Tests.Unit_of_Work
+{
+    [TestFixture]
+    public class UnitOfWorkTests
+    {
+        [Test]
+        public void CanStartUnitOfWork()
+        {
+            IUnitOfWork uow = UnitOfWork.Start();
+        }
+    }
+
+    public static class UnitOfWork
+    {
+        public static IUnitOfWork Current { get; private set; }
+        
+        public static IUnitOfWork Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IUnitOfWork : IDisposable
+    {
+    }
+}
