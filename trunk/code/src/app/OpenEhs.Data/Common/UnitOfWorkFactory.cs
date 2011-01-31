76886096c7e14d0fc1a2b0797bdf72d7e1318079
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-31 15:04:51 +0000

    Still working on the UnitOfWork implementation.

---
+++ trunk/code/src/app/OpenEhs.Data/Common/UnitOfWorkFactory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace OpenEhs.Data
+{
+    public class UnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        private const string DefaultHibernateConfig = "hibernate.cfg.xml";
+        private static ISession _currentSession;
+        private ISessionFactory _sessionFactory;
+        private Configuration _configuration;
+
+        public Configuration Configuration
+        {
+            get
+            {
+                if (_configuration == null)
+                {
+                    _configuration = new Configuration();
+                    string hibernateConfig = DefaultHibernateConfig;
+
+                    // If not rooted, assume path from base directory.
+                    if (Path.IsPathRooted(hibernateConfig) == false)
+                    {
+                        hibernateConfig = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, hibernateConfig);
+                    }
+
+                    if (File.Exists(hibernateConfig))
+                    {
+                        _configuration.Configure(new XmlTextReader(hibernateConfig));
+                    }
+                }
+
+                return _configuration;
+            }
+        }
+
+        public ISession CurrentSession
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public ISessionFactory SessionFactory
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        internal UnitOfWorkFactory()
+        {
+            
+        }
+
+        public IUnitOfWork Create()
+        {
+            ISession session = CreateSession();
+            session.FlushMode = FlushMode.Commit;
+            _currentSession = session;
+
+            return new UnitOfWorkImplementor(this, session);
+        }
+
+        private ISession CreateSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisposeUnitOfWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/code/src/app/OpenEhs.Data/Common/UnitOfWorkImplementor.cs
@@ -0,0 +1,20 @@
+﻿using NHibernate;
+
+namespace OpenEhs.Data
+{
+    public class UnitOfWorkImplementor : IUnitOfWork
+    {
+        private readonly UnitOfWorkFactory _factory;
+        private readonly ISession _session;
+
+        public UnitOfWorkImplementor(UnitOfWorkFactory factory, ISession session)
+        {
+            _factory = factory;
+            _session = session;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
