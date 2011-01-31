9bad61a861747fbe0c2ebcf2cd3f967274367f7a
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-01-31 15:04:51 +0000

    Still working on the UnitOfWork implementation.

--- code/src/tests/OpenEhs.Data.Tests/Unit of Work/UnitOfWorkFactoryTests.cs
+++ code/src/tests/OpenEhs.Data.Tests/Unit of Work/UnitOfWorkFactoryTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NHibernate;
+using NUnit.Framework;
 
 namespace OpenEhs.Data.Tests.Unit_of_Work
 {
@@ -10,6 +12,31 @@ namespace OpenEhs.Data.Tests.Unit_of_Work
         [SetUp]
         public void SetupContext()
         {
+            _factory = (IUnitOfWorkFactory) Activator.CreateInstance(typeof (UnitOfWorkFactory), true);
+        }
+
+        [Test]
+        public void CanCreateUnitOfWork()
+        {
+            IUnitOfWork implementor = _factory.Create();
+            Assert.IsNotNull(implementor);
+            Assert.IsNotNull(_factory.CurrentSession);
+            Assert.AreEqual(FlushMode.Commit, _factory.CurrentSession.FlushMode);
+        }
+
+        [Test]
+        public void CanConfigureNHibernate()
+        {
+            var configuration = _factory.Configuration;
+            Assert.IsNotNull(configuration);
+            Assert.AreEqual("NHibernate.Connection.DriverConnectionProvider",
+                            configuration.Properties["connection.provider"]);
+            Assert.AreEqual("NHibernate.Dialect.MySQLDialect",
+                            configuration.Properties["dialect"]);
+            Assert.AreEqual("NHibernate.Driver.MySqlDataDriver",
+                            configuration.Properties["connection.driver_class"]);
+            Assert.AreEqual("Database=openehs_database;Data Source=localhost;User Id=openehs_admin;Password=password",
+                            configuration.Properties["connection.connection_string"]);
         }
     }
 }
