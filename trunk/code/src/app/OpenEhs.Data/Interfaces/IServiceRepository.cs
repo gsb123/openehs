741fd577658eee4f65e228babc22cb23b761cd98
jdrussell <jdrussell@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-02 17:50:59 +0000

    Added services repository

---
+++ trunk/code/src/app/OpenEhs.Data/Interfaces/IServiceRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenEhs.Domain;
+
+namespace OpenEhs.Data
+{
+    public interface IServiceRepository : IRepository<Service>
+    {
+        IList<Service> GetActiveServices();
+        IList<Service> GetInactiveServices();
+    }
+}
