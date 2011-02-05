4ad418275f33acd1ead50773e8620d224f938d8a
dahlnfarnes <dahlnfarnes@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-05 05:45:16 +0000

    Added Note and Staff Controllers.

---
+++ trunk/code/src/app/OpenEhs.Web/Controllers/NoteController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OpenEhs.Domain;
+using OpenEhs.Data;
+
+namespace OpenEhs.Web.Controllers
+{
+    public class NoteController : Controller
+    {
+        //
+        // GET: /Note/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Note note)
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Note note)
+        {
+            return View();
+        }
+
+        public ActionResult Delete(Note note)
+        {
+            return View();
+        }
+    }
+}
