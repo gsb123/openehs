d211b1d89f74972fe41b01db3637f3783f82a8dd
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-07 17:38:51 +0000

    Fixed mapping many to many

---
+++ code/src/app/OpenEhs.Domain/Model/Patient/PatientProblem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenEhs.Domain.Model.Patient
+{
+    public class PatientProblem : IEntity
+    {
+        #region Fields
+
+        public virtual int Id { get; private set; }
+        public virtual int Problem { get; set; }
+        public virtual int Patient { get; set; }
+
+        #endregion
+    }
+}
