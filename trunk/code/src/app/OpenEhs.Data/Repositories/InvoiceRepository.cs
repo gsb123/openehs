b67df05f7ee58b1a1b03facffdffcce633ea8b5f
peterlitster <peterlitster@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-08 04:51:22 +0000

    Getting an odd error when you click on Billing. I'm in the dark as to why it's looking at patient2_.emergencyContact when I'm trying to get a list of invoices. Maybe because Invoice has a Patient field?

--- trunk/code/src/app/OpenEhs.Data/Repositories/InvoiceRepository.cs
+++ trunk/code/src/app/OpenEhs.Data/Repositories/InvoiceRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using OpenEhs.Domain;
 using NHibernate;
+using NHibernate.Criterion;
 
 namespace OpenEhs.Data
 {
@@ -41,7 +42,22 @@ namespace OpenEhs.Data
 
         public IList<Invoice> FindByPatientId(int PatientId)
         {
+            string q = String.Format("from Invoice where Patient = select * from Patient where Id = {0}", PatientId);
+            IQuery query = Session.CreateQuery(q);
+            return query.List<Invoice>();
+            /*
+            ICriteria criteriaBase = Session.CreateCriteria(typeof(Patient));
+            SimpleExpression simpleBase = NHibernate.Criterion.Expression.Eq("Id", PatientId);
+
+            ICriteria criteria = Session.CreateCriteria(typeof(Invoice));
+            SimpleExpression sim1 = NHibernate.Criterion.Expression.Eq("Patient.Id" ,PatientId);
+            criteria.Add(sim1);
+            Order order = Order.Desc("Total");
+            criteria.AddOrder(order);
+            return criteria.List<Invoice>();
+
             throw new NotImplementedException();
+             */
         }
     }
 }
