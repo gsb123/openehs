ff0119bee438aa2ce7ef9f46eab4ff664191e5a3
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-10 18:00:21 +0000

    Commented out the NHProf line in the Global.asax.cs so that things will run. I was afraid that would happen.

--- trunk/code/src/app/OpenEhs.Web/Global.asax.cs
+++ trunk/code/src/app/OpenEhs.Web/Global.asax.cs
@@ -35,7 +35,7 @@ namespace OpenEhs.Web
             RegisterRoutes(RouteTable.Routes);
 
 #if DEBUG
-            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+            // HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
 #endif
         }
 
