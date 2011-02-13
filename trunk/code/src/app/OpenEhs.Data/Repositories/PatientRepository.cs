de45194d9f51ec4a5ffb807ab1f2749184d73c8b
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-13 03:06:17 +0000

    Added "New" button for the patientcontroller.index view.

--- trunk/code/src/app/OpenEhs.Data/Repositories/PatientRepository.cs
+++ trunk/code/src/app/OpenEhs.Data/Repositories/PatientRepository.cs
@@ -8,8 +8,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using OpenEhs.Domain;
 using NHibernate;
 using NHibernate.Criterion;
@@ -32,30 +30,32 @@ namespace OpenEhs.Data
 
         public void Add(Patient entity)
         {
-            throw new NotImplementedException();
+            Session.Save(entity);
         }
 
         public void Remove(Patient entity)
         {
-            throw new NotImplementedException();
+            Session.Delete(entity);
         }
 
         public IList<Patient> GetAll()
         {
             ICriteria criteria = Session.CreateCriteria<Patient>();
+
             return criteria.List<Patient>();
-            throw new NotImplementedException();
         }
 
         public IList<Patient> FindByPhoneNumber(string phoneNumber)
         {
             ICriteria criteria = Session.CreateCriteria<Patient>().Add(Restrictions.Eq("PhoneNumber", phoneNumber));
+
             return criteria.List<Patient>();
         }
 
         public IList<Patient> FindByName(string name)
         {
-            throw new NotImplementedException();
+            ICriteria criteria = Session.CreateCriteria<Patient>();
+            return null;
         }
 
         public IList<Patient> FindByDateOfBirth(DateTime dateOfBirth)
@@ -66,8 +66,8 @@ namespace OpenEhs.Data
         public IList<Patient> FindByOldPhysicalRecord(int number)
         {
             ICriteria criteria = Session.CreateCriteria<Patient>().Add(Restrictions.Eq("OldPhysicalRecordNumber", number));
+
             return criteria.List<Patient>();
-            //throw new NotImplementedException();
         }
     }
 }
