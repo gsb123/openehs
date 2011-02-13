9fbcb760d4752ecfd1bc58ad3bf87165c12ee139
austynmahoney <austynmahoney@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-13 01:57:25 +0000

    Added intellisense hack to Details.cshtml Added EditAllergy action to PatientController Added ajax POST code to save allergy button in edit allergy dialog (this POST returns a 404 error and if anyone could help me figure out why it can't find the EditAllergy method I added I would appreciate it) Other misc. changes to Details.cshtml

--- code/src/app/OpenEhs.Web/Controllers/PatientController.cs
+++ code/src/app/OpenEhs.Web/Controllers/PatientController.cs
@@ -1,27 +1,52 @@
 ﻿using System.Web.Mvc;
 using OpenEhs.Data;
+using System;
 
 namespace OpenEhs.Web.Controllers {
     public class PatientController : Controller {
-        //
+
         // GET: /Patient/
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             var patients = new PatientRepository().GetAll();
 
             return View(patients);
         }
 
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             return View();
         }
 
-        public ActionResult Details(int id)
-        {
+        public ActionResult Details(int id) {
             var patient = new PatientRepository().Get(id);
 
             return View(patient);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult EditAllergy() {
+            try {
+                int patientID = int.Parse(Request.Form["patientID"]);
+                int allergyID = int.Parse(Request.Form["allergyID"]);
+                string allergyName = Request.Form["allergyName"];
+
+                PatientRepository repo = new PatientRepository();
+                var patient = repo.Get(patientID);
+                patient.Allergies[allergyID].Name = allergyName;
+
+                UnitOfWork.CurrentSession.Flush();
+
+                return Json(new {
+                    error = "false",
+                    status = "Added allergy successfully",
+                    errorMessage = ""
+                });
+            } catch (Exception e) {
+                return Json(new {
+                    error = "false",
+                    status = "Unable to add allergy successfully",
+                    errorMessage = e.Message
+                });
+            }
         }
     }
 }
