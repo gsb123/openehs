506d6c6ebc29d5fcba9f764e65edbdd9c0a854d9
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-13 11:41:37 +0000

    Did a little HTML/CSS work. Didn't get to finish what I was working on. Sick kids.

--- code/src/app/OpenEhs.Data/Repositories/InvoiceRepository.cs
+++ code/src/app/OpenEhs.Data/Repositories/InvoiceRepository.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using OpenEhs.Domain;
 using NHibernate;
-using NHibernate.Criterion;
 
 namespace OpenEhs.Data
 {
