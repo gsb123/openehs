bddd5938cf6b5e1f1ed368d2368d5219e3d2d5a1
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-14 00:42:27 +0000

    Fixed domain and mapping for taking out medication.

--- trunk/code/src/app/OpenEhs.Domain/Model/Patient/Allergy.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Patient/Allergy.cs
@@ -16,7 +16,6 @@ namespace OpenEhs.Domain
 
         public virtual int Id { get; private set; }
         public virtual string Name { get; set; }
-        public virtual string Medication { get; set; }
         public virtual bool IsActive { get; set; }
         public virtual IList<Patient> Patients { get; set; }
 
