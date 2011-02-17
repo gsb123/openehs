0780bf1ae9a7a40c1a826cb2c9590a992e79f548
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-17 17:58:23 +0000

    Started Billing search and moving css and js in different files!

---
+++ trunk/code/src/app/OpenEhs.Web/Scripts/billing.js
@@ -0,0 +1,51 @@
+﻿/// <reference path="jquery-1.4.4.js" />
+/// <reference path="jquery.validate.js" />
+
+$(document).ready(function () {
+
+
+    // ------------------------------------------------- //
+    //  Billing search                                   //
+    // ------------------------------------------------- //
+
+    $(document).ready(function () {
+        $("#CreateNewBillingButton").button();
+
+    });
+
+    $(document).ready(function () {
+        $("#SearchBillingButton").button();
+
+    });
+
+    $(function () {
+        var availableTags = [
+			"Cameron",
+			"Peter",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+        $("#tags").autocomplete({
+            source: availableTags
+        });
+    });
+
+});
