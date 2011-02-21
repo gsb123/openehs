fd2e566cacd64b0b7589ac85c5886b0073912776
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-21 05:32:13 +0000

    New mapping for surgerystaff and surgery. Also updated the js file for history and also some UI changes

---
+++ code/src/app/OpenEhs.Domain/Model/Surgery/CaseType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenEhs.Domain
+{
+    public enum CaseType
+    {
+        Emergency = 0,
+        Elective = 1
+    }
+}
---
+++ code/src/app/OpenEhs.Domain/Model/Surgery/StaffRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenEhs.Domain
+{
+    public enum StaffRole
+    {
+        Surgeon = 0,
+        SurgeonAssistant = 1,
+        Anaesthetist = 2,
+        AnaesthetistAssistant = 3,
+        Nurse = 4,
+        Consultant = 5
+    }
+}
