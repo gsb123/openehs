e67505fc3b2f16946bcc36f7ede2b54081f3662d
austynmahoney <austynmahoney@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-22 04:22:42 +0000

    Intellisense fix

--- code/src/app/OpenEhs.Web/Scripts/patient/details.allergy.js
+++ code/src/app/OpenEhs.Web/Scripts/patient/details.allergy.js
@@ -1,7 +1,7 @@
-﻿/// <reference path="jquery-1.4.4.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.ui.js" />
-/// <reference path="jquery-jvert-tabs-1.1.4.js" />
+﻿/// <reference path="../jquery-1.4.4.js" />
+/// <reference path="../jquery.validate.js" />
+/// <reference path="../jquery.ui.js" />
+/// <reference path="../jquery-jvert-tabs-1.1.4.js" />
 
 $(function () {
     // ------------------------------------------------- //
