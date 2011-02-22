114fe59151b6d71fc67cd5a594b20ebb9c6edd5a
austynmahoney <austynmahoney@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-22 05:14:23 +0000

    Immunizations AJAX

--- trunk/code/src/app/OpenEhs.Web/Scripts/patient/details.immunizations.js
+++ trunk/code/src/app/OpenEhs.Web/Scripts/patient/details.immunizations.js
@@ -1,41 +1,75 @@
-﻿/// <reference path="jquery-1.4.4.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.ui.js" />
-/// <reference path="jquery-jvert-tabs-1.1.4.js" />
+﻿/// <reference path="../jquery-1.4.4.js" />
+/// <reference path="../jquery.validate.js" />
+/// <reference path="../jquery.ui.js" />
+/// <reference path="../jquery-jvert-tabs-1.1.4.js" />
 
 $(function () {
+
     // ------------------------------------------------- //
-    //  Setup Immunizations List Tab                     //
+    //  Setup Rx List Tab                                //
     // ------------------------------------------------- //
     $("#immunizationAddButton").button().click(function () {
-        $("#newImmunizationDialog").dialog("open")
+        $("#newImmunizationDialog").dialog("open");
     });
 
     $("#immunizationInfoLink").click(function () {
         $("#immunizationMoreInfo").slideToggle("slow", function () { });
     });
 
+    $("#addImmunizationForm").validate({
+        errorLabelContainer: $("#newImmunizationDialog .modalErrorContainer"),
+        wrapper: "li",
+        messages: {
+            name: "A name for the immunization is required",
+            dateAdministered: "You must provide the date the immunization was administered"
+        }
+    });
+
     $("#newImmunizationDialog").dialog({
         autoOpen: false,
-        height: 400,
+        height: 300,
         width: 400,
         modal: true,
         buttons: {
             "Save Immunization": function () {
-                $.post("/Patient/AddImmunization", {
-                    patientID: $("#patientId").val(),
-                    vaccinetype: $("#modal_immunizationVaccineType").val(),
-                    dateadministered: $("#modal_immunizationDateAdministered").val()
-                });
-                $(this).dialog("close");
-
+                if ($("#addImmunizationForm").valid()) {
+                    $.post("/Patient/AddImmunization", {
+                        patientID: $("#patientId").val(),
+                        name: $("#modal_immName").val(),
+                        dateAdministered: $("#modal_immAdministered").val()
+                    }, function (response) {
+                        if (response.error == "false") {
+                            $("#noImmunizations").fadeOut();
+                            $("#newImmunizationDialog").dialog("close");
+                            var newImmunization = '<li id="immunization_' + response.immunization.id + '" class="group" style="display:none"><div><b>Vaccine Type: </b>' + response.immunization.name + '</div><div><b>Date Administered: </b>' + response.immunization.dateAdministered + '</div></li>';
+                            $("#immunizationListOne").append(newImmunization);
+                            $("#immunization_" + response.immunization.id).fadeIn();
+                        } else {
+                            alert("Error adding immunization");
+                        }
+                    });
+                    $(this).dialog("close");
+                }
             },
             Cancel: function () {
                 $(this).dialog("close");
             }
         },
         close: function () {
-            allFields.removeClass("ui-state-error");
+            $("#newImmunizationDialog .modalErrorContainer").hide();
+            $('#addImmunizationForm').each(function () {
+                this.reset();
+            });
         }
     });
+
+    $("#addImmunizationForm .datepicker").datepicker({
+        showOn: "button",
+        buttonImage: "/Content/themes/base/images/calendar.png",
+        buttonImageOnly: true,
+        changeYear: true
+    }).focus(function () {
+        $(this).datepicker("show");
+    });
+
 });
