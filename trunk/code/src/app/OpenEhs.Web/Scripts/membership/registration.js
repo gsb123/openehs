b301ee9067d1ae5ff519e17a9e0ff159b706e29c
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-22 07:22:43 +0000

    Got the wizard working on the registration page.

--- trunk/code/src/app/OpenEhs.Web/Scripts/membership/registration.js
+++ trunk/code/src/app/OpenEhs.Web/Scripts/membership/registration.js
@@ -1,13 +1,13 @@
 ﻿$(function () {
-    $("").formwizard({
+    $("#registrationForm").formwizard({
         formPluginEnabled: true,
-        validationEnabled: false,
+        validationEnabled: true,
         focusFirstInput: true,
         formOptions: {
-            success: alert("success"),
-            beforeSubmit: alert("beforeSubmit"),
-            dataType: "json",
+            success: function (data) { $("#status").fadeTo(500, 1, function () { $(this).html("You are now registered!").fadeTo(5000, 0); }) },
+            beforeSubmit: function (data) { $("#data").html("data sent to the server: " + $.param(data)); },
+            dataType: 'json',
             resetForm: true
         }
-    }
+    });
 });
