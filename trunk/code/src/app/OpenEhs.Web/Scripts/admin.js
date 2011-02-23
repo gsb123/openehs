31fb88ad62ee04f79d0ccfaac95f9548358df8fe
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-23 22:07:56 +0000

    Started setting up the admin page... UI is started and the buttons all are connected to te admin.js! Still not done so dont mess with the styling

---
+++ trunk/code/src/app/OpenEhs.Web/Scripts/admin.js
@@ -0,0 +1,64 @@
+﻿/// <reference path="../jquery-1.4.4.js" />
+/// <reference path="../jquery.validate.js" />
+/// <reference path="../jquery.ui.js" />
+/// <reference path="../jquery-jvert-tabs-1.1.4.js" />
+
+$(function () {
+
+    $("#addUser").button().click(function () {
+        alert("Add user button");
+    });
+
+    $("#editUser").button().click(function () {
+        alert("Edit user button");
+    });
+
+    $("#deleteUser").button().click(function () {
+        alert("Delete user button");
+    });
+
+    $("#addProduct").button().click(function () {
+        alert("Add product button");
+    });
+
+    $("#editProduct").button().click(function () {
+        alert("Edit product button");
+    });
+
+    $("#deleteProduct").button().click(function () {
+        alert("Delete product button");
+    });
+
+    $("#editTemplate").button().click(function () {
+        alert("Edit template button");
+    });
+
+    $("#deleteTemplate").button().click(function () {
+        alert("Delete template button");
+    });
+
+    $("#backupUser").button().click(function () {
+        alert("Backup user button");
+    });
+
+    $("#backupTemplates").button().click(function () {
+        alert("Backup templates button");
+    });
+
+    $("#backupPatients").button().click(function () {
+        alert("Backup patients button");
+    });
+
+    $("#backupBilling").button().click(function () {
+        alert("Backup billing button");
+    });
+
+    $("#backupProducts").button().click(function () {
+        alert("Backup products button");
+    });
+
+    $("#backupCheckins").button().click(function () {
+        alert("Backup checkins button");
+    });
+
+});
