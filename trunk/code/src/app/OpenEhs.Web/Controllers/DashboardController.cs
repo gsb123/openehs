057a2405f7d3ea94de4207b97c54c4232e4cac52
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-25 01:51:43 +0000

    Added the dashboard controller.

---
+++ trunk/code/src/app/OpenEhs.Web/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace OpenEhs.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- trunk/code/src/app/OpenEhs.Web/Global.asax.cs
+++ trunk/code/src/app/OpenEhs.Web/Global.asax.cs
@@ -24,7 +24,7 @@ namespace OpenEhs.Web
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
