1fbd4503c8d3d47ce1c64dbc47ad0701654eb2f8
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-25 06:44:23 +0000

    addedd js file for dashboard

--- trunk/code/src/app/OpenEhs.Web/Models/DashboardViewModel.cs
+++ trunk/code/src/app/OpenEhs.Web/Models/DashboardViewModel.cs
@@ -1,6 +1,56 @@
-﻿namespace OpenEhs.Web.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using OpenEhs.Data;
+using OpenEhs.Domain;
+
+namespace OpenEhs.Web.Models
 {
     public class DashboardViewModel
     {
+        private Patient _patient;
+        private Staff _staff;
+
+        [DisplayName("CheckIns")]
+        public IList<PatientCheckIn> PatientCheckIns
+        {
+            get
+            {
+                return _patient.PatientCheckIns;
+            }
+            set
+            {
+                _patient.PatientCheckIns = value;
+            }
+        }
+
+        public PatientCheckIn GetOpenCheckin
+        {
+            get
+            {
+                try
+                {
+                    var query = from checkin in PatientCheckIns
+                                where checkin.CheckOutTime == DateTime.MinValue
+                                select checkin;
+
+                    return query.First<PatientCheckIn>();
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public IList<Location> GetLocations
+        {
+            get
+            {
+                LocationRepository locations = new LocationRepository();
+                return locations.GetAll();
+            }
+        }
     }
 }
