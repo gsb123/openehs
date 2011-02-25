23db2f7cb5a254dd06af68ed04c8c7e1a8ec73d5
cameronharp <cameronharp@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-25 02:15:16 +0000

    mapping changes for new items

--- trunk/code/src/app/OpenEhs.Domain/Model/Patient/Patient.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Patient/Patient.cs
@@ -37,6 +37,7 @@ namespace OpenEhs.Domain
         public virtual string Religion { get; set; }
         public virtual string Note { get; set; }
         public virtual string OldPhysicalRecordNumber { get; set; }
+        public virtual DateTime DateOfDeath { get; set; }
         public virtual bool IsActive { get; set; }
 
         public virtual IList<PatientCheckIn> PatientCheckIns { get; set; }
