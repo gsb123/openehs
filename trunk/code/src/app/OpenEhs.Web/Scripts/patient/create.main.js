507f3f9faa39b6f77c304bca866751e547d994d0
briansneddon <briansneddon@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-27 23:19:17 +0000

    added form entries to js dialog

--- trunk/code/src/app/OpenEhs.Web/Scripts/patient/create.main.js
+++ trunk/code/src/app/OpenEhs.Web/Scripts/patient/create.main.js
@@ -19,7 +19,22 @@ $(function () {
             p_phoneNumber: $("#p_phoneNumber").val(),
             p_bloodType: $("#p_bloodType").val(),
             p_tribeRace: $("#p_tribeRace").val(),
-            p_religion: $("#p_religion").val()
+            p_religion: $("#p_religion").val(),
+            p_address_street1: $("#p_address_street1").val(),
+            p_address_street2: $("#p_address_street2").val(),
+            p_address_City: $("#p_address_City").val(),
+            p_address_Region: $("#p_address_Region").val(),
+            p_address_Country: $("#p_address_Country").val(),
+            emergency_firstName: $("#emergency_firstName").val(),
+            emergency_lastname: $("#emergency_lastname").val(),
+            emergency_relationship: $("#emergency_relationship").val(),
+            emergency_phonenumber: $("#emergency_phonenumber").val(),
+            ec_address_street1: $("#ec_address_street1").val(),
+            ec_address_street2: $("#ec_address_street2").val(),
+            ec_address_City: $("#ec_address_City").val(),
+            ec_address_Region: $("#ec_address_Region").val(),
+            ec_address_Country: $("#ec_address_Country").val(),
+
 
         }, function (response) {
             if (response.error == "false") {
