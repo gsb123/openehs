11f657c03a8279de893329413239835e63e38d23
jdrussell <jdrussell@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-27 00:43:12 +0000

    Surgery staff works!

---
+++ trunk/code/src/app/OpenEhs.Data/Repositories/SurgeryStaffRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenEhs.Domain;
+using NHibernate;
+
+namespace OpenEhs.Data
+{
+    public class SurgeryStaffRepository : ISurgeryStaffRepository
+    {
+        private ISession Session
+        {
+            get
+            {
+                return UnitOfWork.CurrentSession;
+            }
+        }
+        public SurgeryStaff Get(int id)
+        {
+            return Session.Get<SurgeryStaff>(id);
+        }
+        public IList<SurgeryStaff> GetAll()
+        {
+            ICriteria criteria = Session.CreateCriteria<SurgeryStaff>();
+            return criteria.List<SurgeryStaff>();
+        }
+        public void Add(SurgeryStaff entity)
+        {
+            Session.Save(entity);
+        }
+        public void Remove(SurgeryStaff entity)
+        {
+            Session.Delete(entity);
+        }
+    }
+}
