2933f0471ddea1c90c6e2a34822a76064964b3ea
jdrussell <jdrussell@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-27 02:20:33 +0000

    Working on notes...

--- trunk/code/src/app/OpenEhs.Domain/Model/Templates/NoteTemplateCategory.cs
+++ trunk/code/src/app/OpenEhs.Domain/Model/Templates/NoteTemplateCategory.cs
@@ -1,8 +1,11 @@
-﻿namespace OpenEhs.Domain
+﻿using System.Collections.Generic;
+namespace OpenEhs.Domain
 {
     public class NoteTemplateCategory
     {
         public virtual int Id { get; set; }
         public virtual string Name { get; set; }
+        public virtual IList<Template> Templates { get; set; }
+        public virtual IList<Note> Notes { get; set; }
     }
 }
