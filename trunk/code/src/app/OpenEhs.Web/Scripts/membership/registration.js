cc2bdfdcf3d05e9fe34c4ab14dbaac8b8ccd9b0b
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-27 03:57:14 +0000

    Fixed the no current checkin problem in the billing tab. Also working on username checking.

--- trunk/code/src/app/OpenEhs.Web/Scripts/membership/registration.js
+++ trunk/code/src/app/OpenEhs.Web/Scripts/membership/registration.js
@@ -1,4 +1,11 @@
-﻿$(function () {
+﻿/// <reference path="../jquery-1.4.4.js" />
+/// <reference path="../jquery.validate.js" />
+/// <reference path="../jquery.form.wizard-3.0.4.js" />
+/// <reference path="../jquery-ui.js" />
+/// <reference path="../jquery.form.js" />
+
+
+$(function () {
     $("#registrationForm").formwizard({
         formPluginEnabled: true,
         validationEnabled: true,
@@ -10,4 +17,33 @@
             resetForm: true
         }
     });
+
+    $("#lastNameTextbox").blur(function () {
+        if ($("#firstNameTextbox").val() != "") {
+            CheckForUsernameAvailability($("#firstNameTextbox").val() + "." + $("#lastNameTextbox").val());
+        }
+    });
+
+    function CheckForUsernameAvailability(username) {
+        //        $.ajax({
+        //            type: "POST",
+        //            url: "/Account/CheckForUsernameAvailability",
+        //            data: "username=" + username,
+        //            success: function (msg) {
+        //                alert(msg + " is available!");
+        //            }
+        //        });
+
+        alert("I got here.");
+
+        $.post("/Account/CheckForUsernameAvailability",
+                username,
+                function (data) {
+                    alert("Is available!");
+                    $("#Username").val(data.username);
+                    $("#generatedUsername").html(data.username);
+                },
+                "json"
+        );
+    }
 });
