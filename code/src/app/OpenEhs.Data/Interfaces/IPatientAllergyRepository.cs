817904c70f2bba8022d56df719d1bc9dae45bb5c
jdrussell <jdrussell@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-28 23:31:11 +0000

    Repositories for Cameron...

---
+++ code/src/app/OpenEhs.Data/Interfaces/IPatientAllergyRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenEhs.Domain;
+
+namespace OpenEhs.Data
+{
+    interface IPatientAllergyRepository : IRepository<PatientAllergy>
+    {
+    }
+}
