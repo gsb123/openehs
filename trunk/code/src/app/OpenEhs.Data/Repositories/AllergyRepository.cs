1f040f0249c9bc794f8e3188b0220aa309c87102
jdrussell <jdrussell@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-28 23:31:11 +0000

    Repositories for Cameron...

---
+++ trunk/code/src/app/OpenEhs.Data/Repositories/AllergyRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using OpenEhs.Domain;
+
+namespace OpenEhs.Data
+{
+    public class AllergyRepository : IAllergyRepository
+    {
+        private ISession Session
+        {
+            get
+            {
+                return UnitOfWork.CurrentSession;
+            }
+        }
+        public Allergy Get(int id)
+        {
+            return Session.Get<Allergy>(id);
+        }
+        public IList<Allergy> GetAll()
+        {
+            ICriteria criteria = Session.CreateCriteria<Allergy>();
+            return criteria.List<Allergy>();
+        }
+        public void Add(Allergy entity)
+        {
+            Session.Save(entity);
+        }
+        public void Remove(Allergy entity)
+        {
+            Session.Delete(entity);
+        }
+    }
+}
---
+++ trunk/code/src/app/OpenEhs.Data/Repositories/PatientAllergyRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using OpenEhs.Domain;
+
+namespace OpenEhs.Data
+{
+    public class PatientAllergyRepository : IPatientAllergyRepository
+    {
+        private ISession Session
+        {
+            get
+            {
+                return UnitOfWork.CurrentSession;
+            }
+        }
+        public PatientAllergy Get(int id)
+        {
+            return Session.Get<PatientAllergy>(id);
+        }
+        public IList<PatientAllergy> GetAll()
+        {
+            ICriteria criteria = Session.CreateCriteria<PatientAllergy>();
+            return criteria.List<PatientAllergy>();
+        }
+        public void Add(PatientAllergy entity)
+        {
+            Session.Save(entity);
+        }
+        public void Remove(PatientAllergy entity)
+        {
+            Session.Delete(entity);
+        }
+    }
+}
