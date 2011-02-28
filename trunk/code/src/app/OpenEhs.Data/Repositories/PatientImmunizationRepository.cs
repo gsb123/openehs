a3ca0f3fb1a31dce01e559c79a0a38eeb6515023
austynmahoney <austynmahoney@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-02-28 02:48:06 +0000

    Added PatientImmunization repo

---
+++ trunk/code/src/app/OpenEhs.Data/Repositories/PatientImmunizationRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenEhs.Domain;
+using NHibernate;
+
+namespace OpenEhs.Data {
+    public class PatientImmunizationRepository : IPatientImmunizationRepository {
+        private ISession Session {
+            get {
+                return UnitOfWork.CurrentSession;
+            }
+        }
+        public PatientImmunization Get(int id) {
+            return Session.Get<PatientImmunization>(id);
+        }
+        public IList<PatientImmunization> GetAll() {
+            ICriteria criteria = Session.CreateCriteria<PatientImmunization>();
+            return criteria.List<PatientImmunization>();
+        }
+        public void Add(PatientImmunization entity) {
+            Session.Save(entity);
+        }
+        public void Remove(PatientImmunization entity) {
+            Session.Delete(entity);
+        }
+    }
+}
