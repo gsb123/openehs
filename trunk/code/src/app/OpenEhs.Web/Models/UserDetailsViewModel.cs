a20c4ab54f1ebe2d4f2c5233dcdec4bd117e800b
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-03-02 06:53:39 +0000

    Working on user details.

--- trunk/code/src/app/OpenEhs.Web/Models/UserDetailsViewModel.cs
+++ trunk/code/src/app/OpenEhs.Web/Models/UserDetailsViewModel.cs
@@ -1,6 +1,68 @@
-﻿namespace OpenEhs.Web.Models
+﻿using System.Collections.Generic;
+using OpenEhs.Domain;
+
+namespace OpenEhs.Web.Models
 {
     public class UserDetailsViewModel
     {
+        private User _user;
+
+        public string Username
+        {
+            get
+            {
+                return _user.Username;
+            }
+            set
+            {
+                _user.Username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _user.Password;
+            }
+            set
+            {
+                _user.Password = value;
+            }
+        }
+
+        public string EmailAddress
+        {
+            get
+            {
+                return _user.EmailAddress;
+            }
+            set
+            {
+                _user.EmailAddress = value;
+            }
+        }
+
+        public IList<Role> Roles
+        {
+            get { return _user.Roles; }
+        }
+
+        public Staff Staff
+        {
+            get
+            {
+                return _user.Staff;
+            }
+            set
+            {
+                _user.Staff = value;
+            }
+        }
+
+        public UserDetailsViewModel(User user)
+        {
+            _user = user;
+        }
     }
 }
