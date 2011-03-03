7e4d864a1812ace2baeb529724fcd2a6a8599088
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-03-03 05:11:25 +0000

    Moved css and js out to files. Yay!

---
+++ trunk/code/src/app/OpenEhs.Web/Scripts/membership/user.index.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $("#SearchTerm").val("Search").addClass("blurSearchTextbox");
+
+    $("#SearchTerm").focus(function () {
+        $("#SearchTerm").removeClass("blurSearchTextbox");
+        $("#SearchTerm").addClass("focusSearchTextbox");
+        $("#SearchTerm").val("");
+    });
+
+    $("#SearchTerm").blur(function () {
+        $("#SearchTerm").removeClass("focusSearchTextbox");
+        $("#SearchTerm").addClass("blurSearchTextbox");
+        $("#SearchTerm").val("Search");
+    });
+
+    $("#SelectedRole").change(function (data) {
+        alert($("#SelectedRole option:selected").val() + " selected!");
+    });
+
+    $("#UserList tbody tr:odd").addClass("striped");
+
+    $("#UserList tbody tr").click(function () {
+        var href = $(this).find("a").attr("href");
+
+        if (href) {
+            window.location = href;
+        }
+    });
+});
