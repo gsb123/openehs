468339069d58ef773495e5b4380f00844a9a2c14
matthewkimber <matthewkimber@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-03-03 05:05:18 +0000

    Formatting the stupid user details page. I'm hating this project right now. Just FYI.

--- code/src/app/OpenEhs.Web/Scripts/membership/user.details.js
+++ code/src/app/OpenEhs.Web/Scripts/membership/user.details.js
@@ -1,18 +1,10 @@
 ﻿$(document).ready(function () {
     $("#tabs").tabs();
 
-//    $("#AddRoleDialog").dialog({
-//        autoOpen: false,
-//        height: 300,
-//        width: 350,
-//        modal: true,
-//        buttons: {
-//            "Create an account": function () {
-//                    $(this).dialog("close");
-//                }
-//            }
-//        }
-//    });
-
-    $("#AddRoleDialog").dialog();
+    $("#AddRoleDialog").dialog({
+        autoOpen: false,
+        height: 300,
+        width: 350,
+        modal: true
+    });
 });
