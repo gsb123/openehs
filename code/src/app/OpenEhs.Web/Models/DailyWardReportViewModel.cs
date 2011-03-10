0c0d940eb263ee8dd6edc6f8b14d615acbf5d2bd
ryanolson <ryanolson@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-03-10 11:49:02 +0000

    added view model

---
+++ code/src/app/OpenEhs.Web/Models/DailyWardReportViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OpenEhs.Data;
+using OpenEhs.Domain;
+
+namespace OpenEhs.Web.Models
+{
+    public class DailyWardReportViewModel
+    {
+        public DailyWardReportViewModel()
+        {
+            Date = DateTime.Now;
+        }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public IList<Location> Locations
+        {
+            get
+            {
+                var locations = new LocationRepository();
+                return locations.GetAll();
+            }
+        }
+
+    }
+}
