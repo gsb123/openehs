29a6a9e69fc9790cb27a3cc683fbffeac905437b
jdrussell <jdrussell@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-03-10 11:27:30 +0000

    Starting work on marital status.

---
+++ code/src/app/OpenEhs.Domain/Model/Patient/MaritalStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenEhs.Domain
+{
+    public enum MaritalStatus
+    {
+        Single, 
+        Married,
+        Divorced,
+        Widowed
+    }
+}
