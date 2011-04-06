5860d56e6fcabf096513238f8fd0f61dd6134dc7
dahlnfarnes <dahlnfarnes@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-04-06 16:20:20 +0000

    User Report Added

---
+++ trunk/code/src/app/OpenEhs.Domain/Model/Report/Report.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace OpenEhs.Domain
+{
+    public class Report
+    {
+    }
+}
--- trunk/code/src/app/OpenEhs.Web/Controllers/ReportsController.cs
+++ trunk/code/src/app/OpenEhs.Web/Controllers/ReportsController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Web.Mvc;
+using System;
+using OpenEhs.Web.Models;
+using System.Configuration;
+using System.Data;
 
 namespace OpenEhs.Web.Controllers
 {
@@ -13,15 +17,42 @@ namespace OpenEhs.Web.Controllers
             return View();
         }
 
-        public ActionResult DailyWardReport()
+        public ActionResult UserReport()
         {
+            return View(new DataView());
+        }
+
+        public ActionResult UserReport(FormCollection collection)
+        {
+            string selectStatement = "SELECT  u.LastName, " +
+                        "u.Firstname, " +
+                        "COUNT(c.CheckinTime) " +
+                        "FROM user u, patient p, patientcheckin c " +
+                        "WHERE u.UserID = c.UserID AND c.PatientID = p.PatientID AND c.CheckinTime >= @date AND c.CheckinTime <= @date " +
+                        "GROUP BY u.LastName, u.FirstName;";
+
+            string ConnectionString = ConfigurationManager.ConnectionStrings["OpenEhs.ConnectionString"].ConnectionString;
+            DateTime date = Convert.ToDateTime(collection["selectedDate"]);
+
+            MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection(ConnectionString);
+            string select = selectStatement;
+            MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand(select, connection);
+            command.Parameters.AddWithValue("date", date);
+
+            connection.Open();
+            MySql.Data.MySqlClient.MySqlDataReader reader = command.ExecuteReader();
+            System.Data.DataTable tableReport = new System.Data.DataTable();
+            tableReport.Load(reader);
+            connection.Close();
+
+            System.Data.DataView dv1 = new System.Data.DataView(tableReport);
+
             return View();
         }
 
-        public ActionResult PatientReport()
+        public ActionResult LocationReport()
         {
             return View();
         }
-
     }
 }
