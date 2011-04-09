6fe42d4d90e940a4ee227fffdeb2dfb3257ca788
dahlnfarnes <dahlnfarnes@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-04-09 17:59:54 +0000

    User Report, Location Report, and new Report Index page.  Reports are finished. Please let me know if they are not working, and I will dive back into them.

---
+++ trunk/code/src/app/OpenEhs.Web/Models/LocationReportViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace OpenEhs.Web.Models
+{
+    public class LocationReportViewModel
+    {
+        [Required(ErrorMessage = "A date is required for the report")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Report Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime SelectedDate { get; set; }
+
+        public DataView Data { get; set; }
+
+        public LocationReportViewModel(): this(DateTime.Now, null)
+        {}
+
+        public LocationReportViewModel(DateTime selectedDate, DataView data)
+        {
+            SelectedDate = selectedDate;
+            Data = data;
+        }
+    }
+}
