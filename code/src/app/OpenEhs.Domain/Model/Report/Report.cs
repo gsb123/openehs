aa42fa03b04c49dfa4fa648194b010f9f1258aad
jdrussell <jdrussell@addc9a67-b12f-59ed-e811-14e76d19b9d4>
2011-04-11 16:30:08 +0000

    Removing un-used usings...

--- code/src/app/OpenEhs.Domain/Model/Report/Report.cs
+++ code/src/app/OpenEhs.Domain/Model/Report/Report.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Data;
-using MySql.Data.MySqlClient;
-
-namespace OpenEhs.Domain
+﻿namespace OpenEhs.Domain
 {
     public class Report
     {
